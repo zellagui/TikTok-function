cd0a93d5993c5735de270a721034d3d3f989c7a3
Zela <83733867+zellagui@users.noreply.github.com>
2022-05-11 11:02:09 -0400

    Update Reader_#1.py

--- Reader_#1.py
+++ Reader_#1.py
@@ -1,10 +1,13 @@
 import requests
 import json
 import urllib.request
-from moviepy.editor import *
+from distutils.dir_util import copy_tree
+
+
+content_name = '#roblox_159721'  #+-+-+-+-+-+-+-+-+-+-+CHANGE THIS with the new #name+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+
 
 video_time = 0
-time = 500 # change this value for more video ( this is scheduled for video of 7 min)
+time = 550 # change this value for more video ( this is scheduled for video of 7 min)
 
 video_name = None
 video_list = []
@@ -15,15 +18,20 @@ video_dict = {
     'video_download': None
 }
 
-file = '/Users/zela/Documents/Webapp/Ratio-Bot/.idea/Tiktion_functions/video_by#/data_video#/#20884_data_video.json'
+#+-+-+-+-+-+-+-+-+-+-+CHANGE THIS with 3_data_video.json file generated with get_vids#.py+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+
+file = '/Users/zela/Documents/Webapp/Ratio-Bot/.idea/video_by#/data_video#/#roblox_159721/#159721_data_video.json'
 video_path = []
 
 with open(file, 'r') as f:
     data = json.load(f)
 
 for content in data:
+    if(content['video_duration'] > 42):
+        continue
 
     video_time = content['video_duration'] + video_time
+
+
 
     username = content['username']
     video_dict['username'] = username
@@ -40,9 +48,12 @@ for content in data:
     dict_copy = video_dict.copy()
     video_list.append(dict_copy)
 
-    video_name = '/Users/zela/Documents/Webapp/Ratio-Bot/.idea/Tiktion_functions/video_by#/data_video#/Video/video_'+content_id+'.mp4' #change the first part fo another directory [path]
+    video_name = '/Users/zela/Documents/Webapp/Ratio-Bot/.idea/video_by#/data_video#/'+content_name+'/'+ content_id +'.mp4' #change the first part fo another directory [path]
+
+
 
     r = requests.get(video_link, allow_redirects=True)
+
     open(video_name, 'wb').write(r.content)
 
     video_path.append(video_name) #List of all path
@@ -55,8 +66,5 @@ print(len(video_path))
 
 print('The duration of the video is around: ' + str(video_time/60) + " Minutes")
 
-clip1 = VideoFileClip('/Users/zela/Documents/Webapp/Ratio-Bot/.idea/Tiktion_functions/video_6760948081396681989.mp4')
-clip2 = VideoFileClip('/Users/zela/Documents/Webapp/Ratio-Bot/.idea/Tiktion_functions/video_6857645697915751686.mp4')
-
-final_clip = concatenate_videoclips([clip1, clip2])
-final_clip.write_videofile('output.mp4')
+copy_tree("/Users/zela/Documents/Webapp/Ratio-Bot/.idea/video_by#/data_video#", #Copy entir directory to TIKTOP FOLDER (PERSONAL)
+          "/Users/zela/Desktop/TIKTOP/Tiktion_functions/video_by#")
